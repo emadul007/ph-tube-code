34b3b890d256cdabd77fe5f82a086e0957797c6e
Mohammad Emadul Islam <emadul@Mohammads-MacBook-Pro.local>
2024-10-17 13:54:04 +0100

    final final

--- scripts/video.js
+++ scripts/video.js
@@ -12,12 +12,12 @@ const loadCategories = () => {
     .catch((error) =>console.log(error));
 };
 
-const loadVideos = () => {
+const loadVideos = (searchText = "") => {
     //   fetch data
     
     
     
-        fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
+        fetch(`https://openapi.programming-hero.com/api/phero-tube/videos?title=${searchText}`)
         .then((res) => res.json())
         .then((data) => displayVideos (data.videos))
         .catch((error) =>console.log(error));
@@ -195,6 +195,11 @@ categoryContainer.append(buttonContainer);
 
 
 };
+
+
+document.getElementById("search-input").addEventListener("keyup",(e)=>{
+loadVideos(e.target.value);
+});
 loadCategories();
 loadVideos();
 
